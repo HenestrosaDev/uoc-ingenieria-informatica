f63b888e1583b80ac9a2ca381233f099e250dab8
HenestrosaDev <git@henestrosa.dev>
2025-04-09 13:33:00 +0200

    FC: Añade los ejercicio de la PEC2 realizados en VerilUOC junto a sus capturas

---
+++ fundamentos_de_computadores/pec2/ejercicios_veriluoc_pec/20242_PAC2_2A_KM-1.mk
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<project source="3.3.5" version="2.2">
+This file is intended to be loaded by KeMap  (https://eimtveril.uoc.edu/pubver).
+<config>
+    <a name="contents">MC4wX3hYeFh4XzI0MjAyX3hYeFh4X25laHplcG9sal9jQ2NfMQ==</a>
+  </config>
+  <original>
+    <a name="contents">C2VvZi8LMiEyMjIyCzIhMTIyMgszITIxMjILMiEyMjEyCzMhMjExMgsyITEyMjELMyExMTIxCzIhMTIxMQszITExMTELNzIhcS8LMiFjcC8LZSFkIWMhYiFjbWovCzIhcC8LNSFqLwtlZyFmcXp1Lw==-</a>
+  </original>
+  <truetable>
+    <a name="contents">DGZwZzAMMyIzMzMzDDMiMjMzMww0IjMyMzMMMyIzMzIzDDQiMzIyMwwzIjIzMzIMNCIyMjMyDDMiMjMyMgw0IjIyMjIMODMicjAMMyJkcTAMZiJlImQiYyJkbmswDDMicTAMNiJrMAxmaCJncnt2MA==-</a>
+  </truetable>
+  <editkm>
+    <a name="contents">DWdxaDENNCM0NDQ0DTQjMzQ0NA01IzQzNDQNNCM0NDM0DTUjNDMzNA00IzM0NDMNNSMzMzQzDTQjMzQzMw01IzMzMzMNOTQjczENNCNlcjENZyNmI2UjZCNlb2wxDTQjcjENNyNsMQ1naSNoc3x3MQ==-</a>
+  </editkm>
+  <selkm>
+    <a name="contents">DmhyaTIONSQxNTU1DjUkNDExNA41JDUxMTUONiQ1NDU1DjYkNTQ0NQ42JDQ0NTQONiQ0NDQ0Djo1JHQyDjUkZnMyDmgkZyRmJGUkZnBtMg41JHMyDjgkbTIOaGokaXR9eDI=-</a>
+  </selkm>
+  <expression>
+    <a name="contents">D2lzajMPNiUyNjY2DzYlNTIyNQ82JTYyMjYPNyU2NTY2DzclNjU1Ng83JTU1NjUPNyU1NTU1Dzs2JXUzDzYlZ3QzD2klaCVnJWYlZ3FuMw82JXQzDzklbjMPaWslanV+eTM=-</a>
+  </expression>
+</project>
